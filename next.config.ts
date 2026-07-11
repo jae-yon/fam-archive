445b648e091f0cebef5545b7e8fa53193d3daec8
JaeYoung <kimjaeyoung650@gmail.com>
2026-07-12 05:44:10 +0900

    feat: 독립 실행 파일 빌드를 위한 Next.js 설정 추가 및 dotenv-cli 패키지 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 프로젝트를 독립적인 실행 파일로 빌드하기 위해 설정
+  output: "standalone",
 };
 
 export default nextConfig;
